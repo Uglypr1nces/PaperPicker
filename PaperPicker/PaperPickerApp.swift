21f6128d88c5d52708f58d18ae8139b4c65115f0
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.local>
2025-05-17 13:33:44 +0200

    first commit

---
+++ PaperPicker/PaperPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperPickerApp.swift
+//  PaperPicker
+//
+//  Created by Shpat Avdiu on 17.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PaperPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaperPickerTests/PaperPickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaperPickerTests.swift
+//  PaperPickerTests
+//
+//  Created by Shpat Avdiu on 17.05.2025.
+//
+
+import Testing
+@testable import PaperPicker
+
+struct PaperPickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
